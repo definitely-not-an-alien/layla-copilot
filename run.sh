9baf3ce906f575c5782b2d9637f847a8c9f6a3b4
Amos Cheung <69349583+definitely-not-an-alien@users.noreply.github.com>
2026-05-11 01:28:46 +0800

    updated deployment

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-myvenv/bin/python3 -m uvicorn backend.main:app --reload --log-level debug &
+myvenv/bin/python3 -m uvicorn backend.main:app  --host 0.0.0.0 --port 8000 --reload --log-level debug &
 bun run start
 wait -n
 exit $?
