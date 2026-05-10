4ea8cd1b19d4095f9de616e3e5aa6c8725a66183
Amos Cheung <69349583+definitely-not-an-alien@users.noreply.github.com>
2026-05-10 08:16:00 +0800

    updated bash script
    Removed bun installation commands and adjusted script flow.

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-curl -fsSL https://bun.sh/install | bash && ln -s $HOME/.bun/bin/bun /usr/local/bin/bun
-bun install
 python3 -m uvicorn backend.main:app --reload --log-level debug &
 bun run start
 wait -n
